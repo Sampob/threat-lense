e8ba00b22f39aa60676c1690c97a100c0fbc9006
Sampo Bredenberg <sampo.bredenberg@gmail.com>
2024-10-10 23:04:57 +0300

    Simplified state check.

--- app/routes.py
+++ app/routes.py
@@ -8,21 +8,26 @@ def configure_routes(app):
     def search():
         indicator = request.json.get("indicator")
         # Start Celery task
-        task = search_task.apply_async(args=[indicator])
+        task = search_task.delay(indicator)
 
         return jsonify({"task_id": task.id}), 202
 
     @app.route("/search/status/<task_id>", methods=["GET"])
     def get_task_status(task_id):
-        task = AsyncResult(task_id)
-
-        print(task)
-        print(type(task))
-        if task.state == "PENDING":
-            return jsonify({"status": "Pending", "result": None}), 202
-        elif task.state == "SUCCESS":
-            return jsonify({"status": "Completed", "result": task.result}), 200
-        elif task.state == "FAILURE":
-            return jsonify({"status": "Failed", "error": str(task.info)}), 500
+        task_result = AsyncResult(task_id)
+        if task_result.state == "PENDING":
+            response = {
+                "state": task_result.state,
+                "status": "Pending..."
+            }
+        elif task_result.state == "FAILURE":
+            response = {
+                "state": task_result.state,
+                "status": str(task_result.info),
+            }
         else:
-            return jsonify({"status": task.state, "result": None}), 202
+            response = {
+                "state": task_result.state,
+                "result": task_result.result
+            }
+        return jsonify(response)
