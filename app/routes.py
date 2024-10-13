10281041e3072f067c03e86ad589a5d4ca484daf
sampob <sampo.bredenberg@keemail.me>
2024-10-13 19:48:21 +0300

    Logging.

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
 from app.tasks import search_task
+from app.logger import app_logger
 
 from flask import jsonify, request
 
@@ -6,6 +7,9 @@ def configure_routes(app):
     @app.route("/search", methods=["GET"])
     def search():
         indicator = request.json.get("indicator")
+        
+        app_logger.info(f"Flask request for /search, with indicator: '{indicator}")
+        
         # Start Celery task
         task = search_task.delay(indicator)
 
@@ -17,6 +21,9 @@ def configure_routes(app):
     @app.route("/search/status/<task_id>", methods=["GET"])
     def get_task_status(task_id):
         task_result = search_task.AsyncResult(task_id)
+        
+        app_logger.info(f"Flask request for /search/status, with task id: '{task_id}")
+        
         if task_result.state == "PENDING":
             response = {
                 "state": task_result.state,
