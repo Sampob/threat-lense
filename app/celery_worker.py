e273d620153f6b55125a09911b567ddc2dd2cd5e
sampob <sampo.bredenberg@keemail.me>
2024-10-13 19:48:16 +0300

    Using config.

--- app/celery_worker.py
+++ app/celery_worker.py
@@ -1,10 +1,12 @@
 from celery import Celery
 
+from app.config import Config
+
 def make_celery(app_name=__name__):
     celery = Celery(
         app_name,
-        broker="redis://localhost:6379/0",
-        backend="redis://localhost:6379/0",
+        broker=Config.CELERY_BROKER_URL,
+        backend=Config.CELERY_RESULT_BACKEND,
         include=["app.tasks"]
     )
 
