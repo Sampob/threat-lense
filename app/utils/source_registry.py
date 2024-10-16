31357667b6a554e75ae651ae44a5057c87d1bef1
Sampo Bredenberg <sampo.bredenberg@cgi.com>
2024-10-16 15:31:25 +0300

    Excluding BaseSource and room for additional source checks.

--- app/utils/source_registry.py
+++ app/utils/source_registry.py
@@ -16,11 +16,12 @@ class SourceRegistry:
     @classmethod
     def load_sources(cls):
         for filename in os.listdir("app/sources"):
-            if filename.endswith(".py") and filename != "__init__.py":
+            if filename.endswith(".py") and filename != "base_source.py" and filename != "__init__.py":
                 module_name = filename[:-3]  # Strip the .py extension
                 module = importlib.import_module(f"app.sources.{module_name}")
 
                 # Inspect the module for classes
                 for name, obj in inspect.getmembers(module):
                     if inspect.isclass(obj) and issubclass(obj, BaseSource) and obj is not BaseSource:
-                        cls._instances[obj.__name__] = obj()  # Instantiate and store
+                        instance = obj() # Instantiate
+                        cls._instances[obj.__name__] = instance # Store instatiated sources
