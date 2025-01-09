29c40c3f26e66b17c2a7bb89927c85d9659ef161
Sampo <sampo.bredenberg@gmail.com>
2025-01-09 10:01:32 +0200

    Logging and deprecated source removal check.

--- app/__init__.py
+++ app/__init__.py
@@ -45,24 +45,33 @@ def create_app(celery=False) -> Flask:
 def seed_sources():
     source_instances = SourceRegistry.get_instance()
     sources = [{"name": cls.get_name(), "requires_api_key": cls.requires_api_key} for cls in source_instances.values()]
-    
+    all_configured_sources = Source.query.all()
+    if all_configured_sources:
+        app_logger.debug("Found existing db, checking for deprecated sources.")
+        dict_of_names_of_new_sources = {d["name"] for d in sources}
+        sources_not_in_new_sources: list[Source] = [src for src in all_configured_sources if src.name not in dict_of_names_of_new_sources]
+        for s in sources_not_in_new_sources:
+            remove_source = Source.query.filter_by(name=s.name).first()
+            if remove_source:
+                db.session.delete(remove_source)
+                app_logger.warning(f"Removed deprecated source {remove_source.name}.")
+        
     for source_data in sources:
-        existing_source = Source.query.filter_by(name=source_data['name']).first()
-
+        existing_source = Source.query.filter_by(name=source_data["name"]).first()
         if existing_source:
-            print(f"Source '{source_data['name']}' already exists, skipping.")
+            app_logger.info(f"Source '{source_data['name']}' already exists, skipping creating db entry.")
             if existing_source.requires_api_key:
-                api_key = APIKey.query.filter_by(source_name=source_data['name']).first()
+                api_key = APIKey.query.filter_by(source_name=source_data["name"]).first()
                 if api_key:
-                    print(f"  -> API key is already configured for '{source_data['name']}'.")
+                    app_logger.info(f"\t-> API key configured for '{source_data["name"]}'.")
                 else:
-                    print(f"  -> No API key configured for '{source_data['name']}'.")
+                    app_logger.info(f"\t-> No API key configured for '{source_data["name"]}'.")
         else:
             new_source = Source(
-                name=source_data['name'],
-                requires_api_key=source_data['requires_api_key']
+                name=source_data["name"],
+                requires_api_key=source_data["requires_api_key"]
             )
             db.session.add(new_source)
-            print(f"Added new source '{source_data['name']}'.")
+            app_logger.info(f"Added new source '{source_data["name"]}'.")
 
     db.session.commit()
